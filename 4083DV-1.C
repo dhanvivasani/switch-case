8d209b62228f1cf4dc7cd86f7a1f69bcde5fedb3
dhanvivasani <132878983+dhanvivasani@users.noreply.github.com>
2023-05-15 12:54:43 +0530

    Add files via upload

---
+++ 4083DV-1.C
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+
+  int a=5,b=10,c=100;
+   clrscr();
+  if(a<b)
+  {
+    if(a<c)
+    {
+    printf("a is min");
+    }
+    else
+    {
+    printf("c is min");
+    }
+
+  }
+    else
+  {
+    if(b<c)
+    {
+    printf("b is min");
+    }
+    else
+    {
+    printf("c is min");
+    }
+   }
+
+  getch();
+
+  }
